6aa91c8c029ec69f90922a0871551083d1dee1fb
Juan Pablo Combariza <jcombariza@mcclatchyservices.com>
2019-07-27 11:19:34 -0500

    add relevant contact request and feedback repository

--- StepUp/Networking/Repositories/LoggedInUser/LoggedInUserService.swift
+++ StepUp/Networking/Repositories/LoggedInUser/LoggedInUserService.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 typealias RetrieveLoggedInUserCompletion = (Result<LoggedInUser, RequestError>) -> Void
+typealias RetrieveRelevantContactsCompletion = (Result<[Contact], RequestError>) -> Void
 
 protocol LoggedInUserService {
     func retrieveLoggedInUser(_ request: URLRequest?, completion: @escaping RetrieveLoggedInUserCompletion)
     func createNewLocalUser(_ request: URLRequest?, completion: @escaping RetrieveLoggedInUserCompletion)
     func updateLocalUser(_ request: URLRequest?, completion: @escaping RetrieveLoggedInUserCompletion)
+    func retrieveRelevantContacts(_ request: URLRequest?, completion: @escaping RetrieveRelevantContactsCompletion)
 }
